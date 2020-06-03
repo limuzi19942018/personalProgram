434665d358067c788e3b6ddca6658bacb343e12b
liyong <yongli123ad@163.com>
2020-06-03 21:48:53 +0800

    使用注解的方式添加定时任务

---
+++ personalProjectTest/src/main/java/com/example/demo_activity/test1/config/SchedulerTask.java
@@ -0,0 +1,23 @@
+package com.example.demo_activity.test1.config;
+
+import com.example.demo_activity.test1.model.CityName;
+import com.example.demo_activity.test1.service.ICityNameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedulerTask {
+    @Autowired
+    private ICityNameService cityNameService;
+
+        private int count=0;
+
+        @Scheduled(cron="*/40 * * * * ?")
+        private void process(){
+            //System.out.println("this is scheduler task runing  "+(count++));
+            CityName cityNameById = cityNameService.getCityNameById("2");
+            System.out.println(cityNameById+"----------------"+(count++));
+        }
+
+}
