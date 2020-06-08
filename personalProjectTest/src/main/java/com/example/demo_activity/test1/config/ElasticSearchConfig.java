e337c928322d74a096312c789143df5db6cbc0ff
yongli <yongli123ad@163.com>
2020-06-08 18:19:07 +0800

    配置es

---
+++ personalProjectTest/src/main/java/com/example/demo_activity/test1/config/ElasticSearchConfig.java
@@ -0,0 +1,66 @@
+package com.example.demo_activity.test1.config;
+
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.InetAddress;
+
+/**
+ * @Author: yongl
+ * @DATE: 2020/6/8 17:54
+ */
+
+@Configuration
+public class ElasticSearchConfig {
+    private final static Logger LOGGER = LoggerFactory.getLogger(ElasticSearchConfig.class);
+    /**
+     * elk集群地址
+     */
+    @Value("${elasticsearch.ip}")
+    private String hostName;
+
+    /**
+     * 端口
+     */
+    @Value("${elasticsearch.port}")
+    private String port;
+
+    /**
+     * 集群名称
+     */
+    @Value("${elasticsearch.cluster.name}")
+    private String clusterName;
+
+
+    /**
+     * Bean name default  函数名字
+     *
+     * @return
+     */
+    @Bean(name = "transportClient")
+    public TransportClient transportClient() {
+        LOGGER.info("Elasticsearch初始化开始。。。。。");
+        TransportClient transportClient = null;
+        try {
+            // 配置信息//集群名字//增加嗅探机制，找到ES集群
+            Settings esSetting = Settings.builder()
+                    .put("cluster.name", clusterName)
+                    .put("client.transport.sniff", true)
+                    .build();
+            //配置信息Settings自定义
+            transportClient = new PreBuiltTransportClient(esSetting);
+            TransportAddress transportAddress = new TransportAddress(InetAddress.getByName(hostName), Integer.valueOf(port));
+            transportClient.addTransportAddresses(transportAddress);
+        } catch (Exception e) {
+            LOGGER.error("elasticsearch TransportClient create error!!", e);
+        }
+        return transportClient;
+    }
+}
